6a3b66c96eaf260cc84c817c23cffea11431a6b9
Jason Shockness <jshock12@gmail.com>
2019-07-24 13:24:10 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Cholocate", 26, "Male", "Red", "Calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 22, "Male", "Blue", "Rowdy", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 25, "Female", "Purple", "Quiet", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 32, "Male", "Brown", "Obedient", 1);
+INSERT INTO bears VALUES (5, "Melissa", 18, "Female", "Yellow", "Naive", 0);
+INSERT INTO bears VALUES (6, "Grinch", 45, "Male", "Green", "Grumpy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 16, "Female", "Red","Nice", 1);
+INSERT INTO bears VALUES (8, null, 21, "Male", "Black", "Angry", 0);
